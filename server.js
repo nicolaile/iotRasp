205fc41da7a1fef00ab96b3f5ef57d602023ed2a
nicolaile <nicolai.le2@gmail.com>
2017-09-21 15:10:03 +0200

    dsd
    d

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var httpServer = require('./servers/http'),
   resources=require('./resources/model');
 
+  var dhtPlugin = require('./plugins/internal/tempPlugin'); //#A
+
+  // Internal Plugins for sensors/actuators connected to the PI GPIOs
+  // If you test this with real sensors do not forget to set simulate to 'false'
+
+  dhtPlugin.start({'simulate': true, 'frequency': 10000}); //#B
+
 var server = httpServer.listen(resources.pi.port,function(){
   console.info('ITS RUNNING on port %s',
   resources.pi.port);
