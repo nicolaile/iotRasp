8704bc179345fc190e9937da5bffdfca0d4fe317
nicolaile <nicolai.le2@gmail.com>
2017-09-21 14:04:25 +0200

    server
    ff

---
+++ server.js
@@ -0,0 +1,7 @@
+var httpServer = require('./servers/http'),
+  resources=require('.resources/model');
+
+var server = httpServer.listen(resources.pi.port,function(){
+  console.info('ITS RUNNING on port %s',
+  resources.pi.port);
+});
